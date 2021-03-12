c1038f456840e1bc980994c0e6a26f9c58a7c458
Colin Tan <colin.tan@way2web.nl>
2021-03-12 16:24:18 +0100

    WAYDES-28: Fixed class name

--- packages/core/src/components/way-autosuggest/way-autosuggest.tsx
+++ packages/core/src/components/way-autosuggest/way-autosuggest.tsx
@@ -8,7 +8,7 @@ import { renderInputOutsideShadowRoot } from '@/utils/utils';
   styleUrl: 'way-autosuggest.scss',
   shadow: true,
 })
-export class W2wSelect {
+export class W2wAutoselect {
   private optionListEl?: HTMLInputElement;
   private inputEl?: HTMLInputElement;
   @Element() el!: HTMLWayAutosuggestElement;
