3140e61b8413254aaf88de3a4e9e0fd246fac479
Elwin van Eede <elwinvaneede@gmail.com>
2021-04-26 14:34:29 +0200

    WAYDES-17: Custom Properties added to way-select

--- packages/core/src/components/way-select/way-select.tsx
+++ packages/core/src/components/way-select/way-select.tsx
@@ -450,7 +450,7 @@ export class WaySelect {
               </way-button>
             )}
 
-            <span class="select-icon">
+            <span class="caret">
               <svg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'><title>Chevron Down</title><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='48' d='M112 184l144 144 144-144'/></svg>
             </span>
 
