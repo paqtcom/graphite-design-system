ee1388eb0343dbe3ebb5c512700899d589019a9c
Elwin van Eede <elwinvaneede@gmail.com>
2021-11-22 12:01:34 +0100

    Fix e2e test

--- packages/core/src/components/date-picker/date-picker.e2e.ts
+++ packages/core/src/components/date-picker/date-picker.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('date-picker', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<date-picker></date-picker>');
+    await page.setContent('<gr-date-picker></gr-date-picker>');
 
-    const element = await page.find('date-picker');
+    const element = await page.find('gr-date-picker');
     expect(element).toHaveClass('hydrated');
   });
 });
