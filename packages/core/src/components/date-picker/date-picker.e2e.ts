2b93a41bdb3f72744dc6509c73295babfda77807
Elwin van Eede <elwinvaneede@gmail.com>
2022-04-05 14:41:50 +0200

    Expand gr-dropdown e2e tests

--- packages/core/src/components/date-picker/date-picker.e2e.ts
+++ packages/core/src/components/date-picker/date-picker.e2e.ts
@@ -1,7 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('date-picker', () => {
-  it('renders', async () => {
+  it('should render', async () => {
     const page = await newE2EPage();
     await page.setContent('<gr-date-picker></gr-date-picker>');
 
