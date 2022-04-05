0803c7e7599a16a0396126ba51305ee9d044a7c3
Elwin van Eede <elwinvaneede@gmail.com>
2022-04-05 14:22:12 +0200

    Expand gr-date-picker e2e tests

--- packages/core/src/components/date-picker/date-picker.e2e.ts
+++ packages/core/src/components/date-picker/date-picker.e2e.ts
@@ -6,6 +6,36 @@ describe('date-picker', () => {
     await page.setContent('<gr-date-picker></gr-date-picker>');
 
     const element = await page.find('gr-date-picker');
-    expect(element).toHaveClass('hydrated');
+    expect(element).not.toBeNull();
+  });
+
+  it('should emit gr-focus when gaining focus', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gr-date-picker></gr-date-picker>');
+
+    const datepicker = await page.find('gr-date-picker');
+    const grFocus = await datepicker.spyOnEvent('gr-focus');
+
+    await datepicker.click();
+
+    expect(grFocus).toHaveReceivedEventTimes(1);
+  });
+
+  it('should emit gr-blur when losing focus', async () => {
+    const page = await newE2EPage({
+      html: `
+        <gr-date-picker></gr-date-picker>
+        <button>Native Button</button>
+      `,
+    });
+
+    const datepicker = await page.find('gr-date-picker');
+    const nativeButton = await page.find('button');
+    const grBlur = await datepicker.spyOnEvent('gr-blur');
+
+    await datepicker.click();
+    await nativeButton.click();
+
+    expect(grBlur).toHaveReceivedEventTimes(1);
   });
 });
