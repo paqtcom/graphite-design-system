0b824013b618273986f4fff389769da20918873f
Tom Stemerding <tom.stemerding@paqt.com>
2022-04-28 16:42:59 +0200

    Add e2e tests

---
+++ packages/core/src/components/tab-group/tab-group.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('gr-tab-group', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<gr-tab-group></gr-tab-group>');
+
+    const element = await page.find('gr-tab-group');
+    expect(element).toHaveClass('hydrated');
+  });
+});
