80d18babb1c63c77227029deef9ce8de45a639a5
Tom Stemerding <tom.stemerding@paqt.com>
2022-07-06 15:03:31 +0200

    Change tab group test to have class hydrated

--- packages/core/src/components/tab-group/tab-group.e2e.ts
+++ packages/core/src/components/tab-group/tab-group.e2e.ts
@@ -1,11 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('gr-tab-group', () => {
-  it('renders', async () => {
+  it('should render', async () => {
     const page = await newE2EPage();
     await page.setContent('<gr-tab-group></gr-tab-group>');
 
     const element = await page.find('gr-tab-group');
-    expect(element).not.toBeNull();
+    expect(element).toHaveClass('hydrated');
   });
 });
